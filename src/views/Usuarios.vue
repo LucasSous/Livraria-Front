fcb45d28defbcfd39885c4ae9f049ee87384eaae
LucaSous <lucassousadasilva333@gmail.com>
2022-02-06 15:42:57 -0300

    remove loading

--- src/views/Usuarios.vue
+++ src/views/Usuarios.vue
@@ -70,7 +70,6 @@
             <v-col cols="11" style="margin: auto; flex: 1;">
                 <v-data-table
                     style="flex-grow: 1;"
-                    :loading="isLoading"
                     :headers="headers"
                     :items="usuarios"
                     :items-per-page="5"
@@ -234,7 +233,6 @@ export default {
 
     data() {
         return {
-            isLoading: true,
             usuario: {
                 id: '',
                 nome: '',
